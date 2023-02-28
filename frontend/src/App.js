c121523f0e42e8fd59969105fbd410edd232f7d6
Yijia Zhao <zhao.yijia@ufl.edu>
2023-02-28 02:34:59 -0500

    driverdetails example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import DriverDetails from './components/DriverDetails';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import './components/DriverDetails.css';
 
 function App() {
   return (
@@ -12,6 +14,17 @@ function App() {
           <Route path='/' exact />
         </Routes>
       </Router>
+      //driver details example
+      <div>
+        <DriverDetails
+          name="John Smith"
+          contact="555-1234"
+          profilePicture="https://zy-j.com/images/avatar.png"
+          departureTime="9:00am"
+          departureLocation="123 Main St, Anytown, USA"
+          destination="456 Oak Ave, Othertown, USA"
+        />
+      </div>
     </>
   );
 }
