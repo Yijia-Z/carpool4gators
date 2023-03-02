fec9bb24b8ca9a83638d3984fac1a49dc80a7ae1
Yijia Zhao <zhao.yijia@ufl.edu>
2023-03-01 22:10:04 -0500

    all component direct layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
+import Driver from './components/Driver';
+import DriverCreateCarpool from './components/DriverCreateCarpool';
 import DriverDetails from './components/DriverDetails';
+import LogIn from './components/LogIn';
+import Register from './components/Register';
+import SearchCarpool from './components/SearchCarpool';
+import Trip from './components/Trip';
 import SignUp from './components/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -17,6 +23,12 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <Driver />
+        <DriverCreateCarpool />
+        <LogIn />
+        <Register />
+        <SearchCarpool />
+        <Trip />
         <Routes>
           <Route path='/' exact />
           <Route path='/sign-up' element={<SignUp />} />
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './LogIn.css';
 
 function Login() {
     const [phoneNumber, setPhoneNumber] = useState('');
