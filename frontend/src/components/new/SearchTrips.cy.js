b030043b8be75be057e2937cff99a1c969855419
Yijia Zhao <zhao.yijia@ufl.edu>
2023-03-29 22:53:32 -0400

    old & cypress

---
+++ frontend/src/components/new/SearchTrips.cy.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount } from 'cypress/react';
+import axios from 'axios';
+import SearchTrips from './SearchTrips';
+
+describe('SearchTrips Component', () => {
+    beforeEach(() => {
+        mount(<SearchTrips />);
+    });
+
+    it('renders the form elements', () => {
+        cy.get('form').should('exist');
+        cy.get('input[type="text"]').should('have.length', 2);
+        cy.get('input[type="date"]').should('exist');
+        cy.get('input[type="number"]').should('exist');
+        cy.get('button[type="submit"]').should('exist');
+    });
+
+    it('handles form submission and displays search results', () => {
+        cy.intercept('GET', '/api/trips', { fixture: 'trips.json' }).as('searchTrips');
+        cy.get('input[type="text"]').eq(0).type('StartCity');
+        cy.get('input[type="text"]').eq(1).type('DestinationCity');
+        cy.get('input[type="date"]').type('2023-04-15');
+        cy.get('input[type="number"]').type('2');
+        cy.get('button[type="submit"]').click();
+
+        cy.wait('@searchTrips').then(({ request, response }) => {
+            expect(request.url).to.contain('/api/trips');
+            expect(request.url).to.contain('start=StartCity');
+            expect(request.url).to.contain('destination=DestinationCity');
+            expect(request.url).to.contain('date=2023-04-15');
+            expect(request.url).to.contain('seat_count=2');
+            expect(response.statusCode).to.equal(200);
+        });
+
+        cy.get('ul li').should('have.length', 2);
+    });
+});
--- frontend/src/components/new/UserLogin.cy.js
+++ frontend/src/components/new/UserLogin.cy.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import { mount } from 'cypress/react';
 import axios from 'axios';
-import UserRegister from './UserRegister.js';
+import UserLogin from './UserLogin';
 
-describe('UserRegister Component', () => {
+describe('UserLogin Component', () => {
     beforeEach(() => {
-        mount(<UserRegister />);
+        mount(<UserLogin />);
     });
 
     it('renders the form elements', () => {
         cy.get('form').should('exist');
-        cy.get('input[type="text"]').should('have.length', 2);
+        cy.get('input[type="text"]').should('exist');
         cy.get('input[type="password"]').should('exist');
-        cy.get('input[type="email"]').should('exist');
         cy.get('button[type="submit"]').should('exist');
     });
 
     it('handles form submission', () => {
-        cy.intercept('POST', '/api/user/add_user', { fixture: 'user_registered.json' }).as('registerUser');
-        cy.get('input[type="text"]').eq(0).type('testUsername');
+        cy.intercept('POST', '/api/user/login', { fixture: 'user_logged_in.json' }).as('loginUser');
+        cy.get('input[type="text"]').type('testUsername');
         cy.get('input[type="password"]').type('testPassword');
-        cy.get('input[type="email"]').type('test@example.com');
-        cy.get('input[type="text"]').eq(1).type('1234567890');
         cy.get('button[type="submit"]').click();
 
-        cy.wait('@registerUser').then(({ request, response }) => {
+        cy.wait('@loginUser').then(({ request, response }) => {
             expect(request.body).to.deep.equal({
                 username: 'testUsername',
                 password: 'testPassword',
-                email: 'test@example.com',
-                phone: '1234567890',
             });
             expect(response.statusCode).to.equal(200);
         });
 
         cy.on('window:alert', (message) => {
-            expect(message).to.equal('User registered successfully');
+            expect(message).to.equal('User logged in successfully');
         });
     });
 });
